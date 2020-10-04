81a31a5f57b17da6431e3a9b3857e6fa403ba2fe
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2020-10-05 01:17:12 +0530

    Updated img for slider

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -13,14 +13,52 @@ const Home = () => {
         <h2>Header file</h2>
         <Swiper
           spaceBetween={50}
-          slidesPerView={3}
+          slidesPerView={1}
           onSlideChange={() => console.log("slide change")}
           onSwiper={(swiper) => console.log(swiper)}
         >
-          <SwiperSlide>Slide 1</SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://birparacollege.ac.in/wp-content/uploads/2020/08/IMG-20200808-WA0006.jpg"
+              alt="7"
+              title="7"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://birparacollege.ac.in/wp-content/uploads/2018/06/3.jpg"
+              alt="Student"
+              title="Student"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://birparacollege.ac.in/wp-content/uploads/2020/08/Staff-1400X400.jpg"
+              alt="College Staff"
+              title="College Staff"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://birparacollege.ac.in/wp-content/uploads/2020/08/Galaxy-IT-Station_Birpara-College_Web02_J2-App.jpg"
+              alt="02_Birpara College_App"
+              title="02_Birpara College_App"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://birparacollege.ac.in/wp-content/uploads/2020/08/Galaxy-IT-Station_Birpara-College_Web01.png"
+              alt="01_Birpara College_Web 001"
+              title="01_Birpara College_Web 001"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://birparacollege.ac.in/wp-content/uploads/2020/07/BC-WEB-IGNOU-2020-2-1.jpg"
+              alt="02_BC WEB IGNOU"
+              title="02_BC WEB IGNOU"
+            />
+          </SwiperSlide>
         </Swiper>
         {/* <Slider {...settings} style={{ width: "100%", heigth: "100%" }}>
           <div>
